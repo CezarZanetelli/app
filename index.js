9828f37c76606d95bb9b3767ed56d1f8a0c84cc4
Cezar <cezinhabm@hotmail.com>
2024-10-07 13:29:33 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 // Hello World 
 // Variável Global
 const mensagem = "olá eu!"
@@ -10,3 +11,25 @@ const mensagem = "olá eu!"
 
 console.log(mensagem);
 // pode ser números tambem, ex: ("2 1.4")
+*/
+
+// Arrays, objetos
+//let metas = ["Cezar", "Alo"]
+
+//console.log(metas[1] + ", " + metas[0])
+
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+    isChecked: (info) => {
+    console.log(info) 
+  }
+}  
+
+//meta.isChecked ou meta.log -> os dois funcionam
+meta.isChecked(meta.value)
+
+// function // arrow function
+const criarMeta = () => {}
+
+
