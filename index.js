27ea57b50f49c761a2eaed8ebd006db13044a76c
Cezar <cezinhabm@hotmail.com>
2024-10-07 13:59:25 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -65,13 +65,33 @@ let metas = [
 console.log(metas[0].value)
 */
 
-
+/*
 const start = () => {
     let count = 0
     //count 0 até 9 / <= 1 até 10
     while(count < 10){
         console.log(count)
         count = count + 1
+    }
+}
+
+start()
+*/
+
+const start = () => {
+
+    while(true){
+     let opcao = "cadastrar"
+     switch(opcao) {
+        case "sair":
+            console.log("vamos cadastrar")
+            break
+            case "listar":
+                console.log("vamos listar")
+                break
+                case "sair":
+                    return
+     }
     }
 }
 
