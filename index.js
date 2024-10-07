67725e918078e7cfcacd2b92c68b8f7ee4e27859
Cezar <cezinhabm@hotmail.com>
2024-10-07 16:29:23 -0300

    menu de aplicação

--- index.js
+++ index.js
@@ -78,21 +78,45 @@ const start = () => {
 start()
 */
 
-const start = () => {
+
+// select = caixinha de seleção
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
 
     while(true){
-     let opcao = "cadastrar"
+
+const opcao = await select({
+    message: "Menu >",
+    choices: [
+        {
+    name: "Cadastrar meta",
+    value: "cadastrar"
+        },
+        {
+            name: "Listar metas",
+            value: "listar"  
+        },
+        {
+            name: "Sair",
+            value: "sair"
+        }
+    ]
+})
+
      switch(opcao) {
-        case "sair":
+        case "cadastrar":
             console.log("vamos cadastrar")
             break
             case "listar":
                 console.log("vamos listar")
                 break
                 case "sair":
+                    console.log("Até a próxima!")
                     return
      }
     }
 }
 
 start()
+
